6a249a1c448ec7a109c70a5d0e149ebad2cc8383
Chris Pfaff <chrispfaff10@gmail.com>
2017-11-23 09:41:35 -0500

    added historyApiFallback in webpack

--- src/components/container/NotFound.js
+++ src/components/container/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className="404-error block">
       <h2>Not Found</h2>
-      <img src="https://i.pinimg.com/736x/5c/97/c8/5c97c8c2afa2cde40aa6330266389d90--anime-meme-funny-dogs.jpg" alt="Dog getting hit with frisbee" />
+      <img className="not-found-dog"src="https://i.pinimg.com/736x/5c/97/c8/5c97c8c2afa2cde40aa6330266389d90--anime-meme-funny-dogs.jpg" alt="Dog getting hit with frisbee" />
     </div>
   );
 };
