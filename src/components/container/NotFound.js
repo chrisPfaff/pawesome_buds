532de5537ef6f90be27bfe5b885be5b77b185dae
Greg Pfaff <greg.pfaff@razorfish.com>
2017-12-02 12:12:12 -0600

    adding 404 image page fix to not use url

--- src/components/container/NotFound.js
+++ src/components/container/NotFound.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 const NotFound = () => {
+
+  const error_image = require('../../img/404.jpg');
+
   return (
     <div className="404-error block">
       <h2>Oops try again</h2>
-      <img className="not-found-dog"src="https://i.pinimg.com/736x/5c/97/c8/5c97c8c2afa2cde40aa6330266389d90--anime-meme-funny-dogs.jpg" alt="Dog getting hit with frisbee" />
+      <img className="not-found-dog"src={error_image} />
     </div>
   );
 };
