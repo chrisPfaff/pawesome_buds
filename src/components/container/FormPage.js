0fc4cc188f04f735990ec1b86a53403b7c65c0e2
Greg Pfaff <greg.pfaff@razorfish.com>
2017-12-02 11:15:21 -0600

    adding form component

--- src/components/container/FormPage.js
+++ src/components/container/FormPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import AddDog from "../presentational/AddDog";
 class FormPage extends Component {
   render() {
     return (
       <form className="signup-form block">
         <h2 className="signup-form--header">Signup Your Pooch Today!</h2>
+        <AddDog message="Cooper and Rosie" />
         <Link to="/about" className="button">Learn More </Link>
       </form>
     );
